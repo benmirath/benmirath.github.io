27ca321c7818aa184ae2b8a4e4dce72d6c37c186
Ben Miller <bjmillers@gmail.com>
2015-09-11 12:10:20 -0400

    Trying github pages

--- GamExplorer/README.md
+++
@@ -1,4 +0,0 @@
-GamExplorer
-===========
-
-Customizable data viz that utilizes d3.js and the giant bomb api.
---
+++ old/GamExplorer/js/examples/basic.js
@@ -0,0 +1,94 @@
+// API callback
+Tabletop.callbacks.tt141677952490643622({
+    "version": "1.0",
+    "encoding": "UTF-8",
+    "feed": {
+        "xmlns": "http://www.w3.org/2005/Atom",
+        "xmlns$openSearch": "http://a9.com/-/spec/opensearchrss/1.0/",
+        "xmlns$gs": "http://schemas.google.com/spreadsheets/2006",
+        "id": {
+            "$t": "https://spreadsheets.google.com/feeds/worksheets/0ArF9tD_naD7edHIyOEE3endKbzZmZExTdWVoT0xTTmc/public/basic"
+        },
+        "updated": {
+            "$t": "2013-02-20T16:40:17.256Z"
+        },
+        "category": [{
+            "scheme": "http://schemas.google.com/spreadsheets/2006",
+            "term": "http://schemas.google.com/spreadsheets/2006#worksheet"
+        }
+        ],
+        "title": {
+            "type": "text",
+            "$t": "video game names"
+        },
+        "link": [{
+            "rel": "alternate",
+            "type": "text/html",
+            "href": "https://spreadsheets.google.com/pub?key\u003d0ArF9tD_naD7edHIyOEE3endKbzZmZExTdWVoT0xTTmc"
+        }, {
+            "rel": "http://schemas.google.com/g/2005#feed",
+            "type": "application/atom+xml",
+            "href": "https://spreadsheets.google.com/feeds/worksheets/0ArF9tD_naD7edHIyOEE3endKbzZmZExTdWVoT0xTTmc/public/basic"
+        }, {
+            "rel": "self",
+            "type": "application/atom+xml",
+            "href": "https://spreadsheets.google.com/feeds/worksheets/0ArF9tD_naD7edHIyOEE3endKbzZmZExTdWVoT0xTTmc/public/basic?alt\u003djson-in-script"
+        }
+        ],
+        "author": [{
+            "name": {
+                "$t": "feilding.cage"
+            },
+            "email": {
+                "$t": "feilding.cage@guardian.co.uk"
+            }
+        }
+        ],
+        "openSearch$totalResults": {
+            "$t": "1"
+        },
+        "openSearch$startIndex": {
+            "$t": "1"
+        },
+        "entry": [{
+            "id": {
+                "$t": "https://spreadsheets.google.com/feeds/worksheets/0ArF9tD_naD7edHIyOEE3endKbzZmZExTdWVoT0xTTmc/public/basic/od4"
+            },
+            "updated": {
+                "$t": "2013-02-20T16:40:17.256Z"
+            },
+            "category": [{
+                "scheme": "http://schemas.google.com/spreadsheets/2006",
+                "term": "http://schemas.google.com/spreadsheets/2006#worksheet"
+            }
+            ],
+            "title": {
+                "type": "text",
+                "$t": "Sheet3"
+            },
+            "content": {
+                "type": "text",
+                "$t": "Sheet3"
+            },
+            "link": [{
+                "rel": "http://schemas.google.com/spreadsheets/2006#listfeed",
+                "type": "application/atom+xml",
+                "href": "https://spreadsheets.google.com/feeds/list/0ArF9tD_naD7edHIyOEE3endKbzZmZExTdWVoT0xTTmc/od4/public/basic"
+            }, {
+                "rel": "http://schemas.google.com/spreadsheets/2006#cellsfeed",
+                "type": "application/atom+xml",
+                "href": "https://spreadsheets.google.com/feeds/cells/0ArF9tD_naD7edHIyOEE3endKbzZmZExTdWVoT0xTTmc/od4/public/basic"
+            }, {
+                "rel": "http://schemas.google.com/visualization/2008#visualizationApi",
+                "type": "application/atom+xml",
+                "href": "https://spreadsheets.google.com/tq?key\u003d0ArF9tD_naD7edHIyOEE3endKbzZmZExTdWVoT0xTTmc\u0026sheet\u003dod4\u0026pub\u003d1"
+            }, {
+                "rel": "self",
+                "type": "application/atom+xml",
+                "href": "https://spreadsheets.google.com/feeds/worksheets/0ArF9tD_naD7edHIyOEE3endKbzZmZExTdWVoT0xTTmc/public/basic/od4"
+            }
+            ]
+        }
+        ]
+    }
+});
